d772c3a7785bb9958832be679cfc9397d3ff3b7c
Pistak41 <66039497+Pistak41@users.noreply.github.com>
2021-04-24 20:29:53 -0300

    Add files via upload

--- app/assets/js/scripts/settings.js
+++ app/assets/js/scripts/settings.js
@@ -1256,73 +1256,6 @@ function prepareAboutTab(){
  * Update Tab
  */
 
-const settingsTabUpdate            = document.getElementById('settingsTabUpdate')
-const settingsUpdateTitle          = document.getElementById('settingsUpdateTitle')
-const settingsUpdateVersionCheck   = document.getElementById('settingsUpdateVersionCheck')
-const settingsUpdateVersionTitle   = document.getElementById('settingsUpdateVersionTitle')
-const settingsUpdateVersionValue   = document.getElementById('settingsUpdateVersionValue')
-const settingsUpdateChangelogTitle = settingsTabUpdate.getElementsByClassName('settingsChangelogTitle')[0]
-const settingsUpdateChangelogText  = settingsTabUpdate.getElementsByClassName('settingsChangelogText')[0]
-const settingsUpdateChangelogCont  = settingsTabUpdate.getElementsByClassName('settingsChangelogContainer')[0]
-const settingsUpdateActionButton   = document.getElementById('settingsUpdateActionButton')
-
-/**
- * Update the properties of the update action button.
- *
- * @param {string} text The new button text.
- * @param {boolean} disabled Optional. Disable or enable the button
- * @param {function} handler Optional. New button event handler.
- */
-function settingsUpdateButtonStatus(text, disabled = false, handler = null){
-    settingsUpdateActionButton.innerHTML = text
-    settingsUpdateActionButton.disabled = disabled
-    if(handler != null){
-        settingsUpdateActionButton.onclick = handler
-    }
-}
-
-/**
- * Populate the update tab with relevant information.
- *
- * @param {Object} data The update data.
- */
-function populateSettingsUpdateInformation(data){
-    if(data != null){
-        settingsUpdateTitle.innerHTML = `Nueva ${isPrerelease(data.version) ? 'Pre-release' : 'Release'} Disponible`
-        settingsUpdateChangelogCont.style.display = null
-        settingsUpdateChangelogTitle.innerHTML = data.releaseName
-        settingsUpdateChangelogText.innerHTML = data.releaseNotes
-        populateVersionInformation(data.version, settingsUpdateVersionValue, settingsUpdateVersionTitle, settingsUpdateVersionCheck)
-
-        if(process.platform === 'darwin'){
-            settingsUpdateButtonStatus('Descargar de Github<span style="font-size: 10px;color: gray;text-shadow: none !important;">Close the launcher and run the dmg to update.</span>', false, () => {
-                shell.openExternal(data.darwindownload)
-            })
-        } else {
-            settingsUpdateButtonStatus('Descargando..', true)
-        }
-    } else {
-        settingsUpdateTitle.innerHTML = 'Estas en la version mas reciente'
-        settingsUpdateChangelogCont.style.display = 'none'
-        populateVersionInformation(remote.app.getVersion(), settingsUpdateVersionValue, settingsUpdateVersionTitle, settingsUpdateVersionCheck)
-        settingsUpdateButtonStatus('Revisar si hay actualizaciones', false, () => {
-            if(!isDev){
-                ipcRenderer.send('autoUpdateAction', 'checkForUpdate')
-                settingsUpdateButtonStatus('Revisando si hay actualizaciones...', true)
-            }
-        })
-    }
-}
-
-/**
- * Prepare update tab for display.
- *
- * @param {Object} data The update data.
- */
-function prepareUpdateTab(data = null){
-    populateSettingsUpdateInformation(data)
-}
-
 /**
  * Settings preparation functions.
  */
@@ -1336,7 +1269,6 @@ function prepareSettings(first = false) {
     if(first){
         setupSettingsTabs()
         initSettingsValidators()
-        prepareUpdateTab()
     } else {
         prepareModsTab()
     }
